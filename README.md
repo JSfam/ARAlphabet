c0a2f0c1903534c3acd3a0b40f033791c67bc335
JSdev <jsdev.fams@gmail.com>
2021-03-23 11:07:27 +0800

    1st push

--- README.md
+++ README.md
@@ -1,16 +1,6 @@
 # AR-Alphabets
 Web Augmented Reality app for learning Alphabet words for Kids using AR.js and A-frame
 
-![Demo](https://github.com/prashant-andani/AR-Alphabets-words/blob/master/ARDemo.gif?raw=true)
-
-## Getting Started
-
-Clone the repository or download and run the index.html in local server.
-for ex: You can create local server with python.  "python -m SimpleHTTPServer 8000"
-
-### Prerequisites
-An AR marker to scan
-for better experience, print the marker. [Link to the Marker](https://github.com/prashant-andani/AR-Alphabets-words/blob/master/assets/pattern-letterG.png?raw=true)
 
 ### Built With
 
@@ -19,29 +9,3 @@ for better experience, print the marker. [Link to the Marker](https://github.com
 * [Sketch-fab](https://sketchfab.com) - 3D models
 * [Google Ploy](https://poly.google.com) - 3D models
 
-### Authors
-
-* **Prashant Andani** -  - [Github](https://prashant-andani.github.io) | [Twitter](https://twitter.com/prashant_andani)
-
-### Contributors
-Contributions are most welcome
-* Raise an issue
-* Suggest a feature
-* Raise a PR
-
-
-### Acknowledgments
-
-* Hat tip to [Jerome Etienne](https://jeromeetienne.github.io)
-* Inspiration - Steve Jobs, Elon Musk
-
-### License
-This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
-
-
-### Screenshots
-
-![Lion](https://github.com/prashant-andani/AR-Alphabets-words/blob/master/assets/Lion.jpeg?raw=true)
-![Whale](https://github.com/prashant-andani/AR-Alphabets-words/blob/master/assets/Whale.jpeg?raw=true)
-
-![letterG](https://github.com/prashant-andani/AR-Alphabets-words/blob/master/assets/pattern-letterG.png?raw=true)
--- main.js
+++ main.js
@@ -1,300 +1,294 @@
-var data = [{
+var data = [
+  {
     id: 1,
-    title: 'A',
-    description: 'Axe',
-    assetId: 'axe_asset',
-    scale: '0.1 0.1 0.1',
-    scaleOne: '0.3 0.3 0.3',
-    scaleTwo: '0.5 0.5 0.5',
-    isObj: false
+    title: "A",
+    description: "Apple",
+    assetId: "apple_asset",
+    scale: "0.1 0.1 0.1",
+    scaleOne: "0.1 0.1 0.1",
+    scaleTwo: "0.1 0.1 0.1",
+    position: "1 .8 1",
+    isObj: false,
   },
   {
     id: 2,
-    title: 'B',
-    description: 'Boat',
-    assetId: 'boat_asset',
-    scale: '4 4 4',
-    scaleOne: '6 6 6',
-    scaleTwo: '8 8 8',
-    isObj: false
+    title: "B",
+    description: "Basketball",
+    assetId: "ball_asset",
+    scale: "0.30 0.30 0.30",
+    scaleOne: "0.1 0.1 0.1",
+    scaleTwo: "0.1 0.1 0.1",
+    isObj: false,
   },
   {
     id: 3,
-    title: 'C',
-    description: 'Cow',
-    assetId: 'cow_asset',
-    scale: '0.1 0.1 0.1',
-    scaleOne: '0.15 0.15 0.15',
-    scaleTwo: '0.17 0.17 0.17',
-    isObj: false
+    title: "C",
+    description: "Cat",
+    assetId: "cat_asset",
+    scale: "0.80 0.80 0.80",
+    scaleOne: "0.1 0.1 0.1",
+    scaleTwo: "0.1 0.1 0.1",
+    isObj: false,
   },
   {
     id: 4,
-    title: 'D',
-    description: 'Deer',
-    assetId: 'deer_asset',
-    scale: '0.8 0.8 0.8',
-    scaleOne: '0.9 0.9 0.9',
-    scaleTwo: '1.1 1.1 1.1',
-    isObj: false
+    title: "D",
+    description: "Dog",
+    assetId: "dog_asset",
+    scale: "0.8 0.8 0.8",
+    scaleOne: "0.9 0.9 0.9",
+    scaleTwo: "1.1 1.1 1.1",
+    isObj: false,
   },
   {
     id: 5,
-    title: 'E',
-    description: 'Earth',
-    assetId: 'earth_asset',
-    scale: '0.2 0.2 0.2',
-    scaleOne: '0.4 0.4 0.4',
-    scaleTwo: '0.6 0.6 0.6',
-    isObj: false
+    title: "E",
+    description: "Eagle",
+    assetId: "eagle_asset",
+    scale: "0.2 0.2 0.2",
+    scaleOne: "0.4 0.4 0.4",
+    scaleTwo: "0.6 0.6 0.6",
+    isObj: false,
   },
   {
     id: 6,
-    title: 'F',
-    description: 'Flower',
-    assetId: 'flower_asset',
-    scale: '0.09 0.09 0.09',
-    scaleOne: '0.1 0.1 0.1',
-    scaleTwo: '0.12 0.12 0.12',
-    isObj: false
+    title: "F",
+    description: "Flower",
+    assetId: "flower_asset",
+    scale: "0.09 0.09 0.09",
+    scaleOne: "0.1 0.1 0.1",
+    scaleTwo: "0.12 0.12 0.12",
+    isObj: false,
   },
   {
     id: 7,
-    title: 'G',
-    description: 'Goat',
-    objAsset: 'goat_asset_obj',
-    mtlAsset: 'goat_asset_mtl',
-    scale: '0.009 0.009 0.009',
-    scaleOne: '0.014 0.014 0.014',
-    scaleTwo: '0.016 0.016 0.016',
-    isObj: true
+    title: "G",
+    description: "Goat",
+    // assetId: "goat_asset",
+    objAsset: "goat_asset_obj",
+    mtlAsset: "goat_asset_mtl",
+    scale: "0.009 0.009 0.009",
+    scaleOne: "0.014 0.014 0.014",
+    scaleTwo: "0.016 0.016 0.016",
+    isObj: true,
   },
   {
     id: 8,
-    title: 'H',
-    description: 'Headphone',
-    objAsset: 'headphone_asset_obj',
-    mtlAsset: 'headphone_asset_mtl',
-    scale: '0.02 0.02 0.02',
-    scaleOne: '0.04 0.04 0.04',
-    scaleTwo: '0.06 0.06 0.06',
-    isObj: true
+    title: "H",
+    description: "Headphone",
+    assetId: "headphone_asset",
+    objAsset: "headphone_asset_obj",
+    mtlAsset: "headphone_asset_mtl",
+    scale: "0.02 0.02 0.02",
+    scaleOne: "0.04 0.04 0.04",
+    scaleTwo: "0.06 0.06 0.06",
+    isObj: true,
   },
   {
     id: 9,
-    title: 'I',
-    description: 'Iphone',
-    assetId: 'iphone_asset',
-    scale: '0.6 0.6 0.6',
-    scaleOne: '0.8 0.8 0.8',
-    scaleTwo: '1 1 1',
-    isObj: false
+    title: "I",
+    description: "Iphone",
+    assetId: "iphone_asset",
+    scale: "0.6 0.6 0.6",
+    scaleOne: "0.8 0.8 0.8",
+    scaleTwo: "1 1 1",
+    isObj: false,
   },
   {
     id: 10,
-    title: 'J',
-    description: 'Jaguar',
-    assetId: 'jaguar_asset',
-    scale: '0.02 0.02 0.02',
-    scaleOne: '0.03 0.03 0.03',
-    scaleTwo: '0.04 0.04 0.04',
-    isObj: false
+    title: "J",
+    description: "Jaguar",
+    assetId: "jaguar_asset",
+    scale: "0.02 0.02 0.02",
+    scaleOne: "0.03 0.03 0.03",
+    scaleTwo: "0.04 0.04 0.04",
+    isObj: false,
   },
   {
     id: 11,
-    title: 'K',
-    description: 'Knife',
-    assetId: 'knife_asset',
-    scale: '0.09 0.09 0.09',
-    scaleOne: '0.1 0.1 0.1',
-    scaleTwo: '0.13 0.13 0.13',
-    isObj: false
+    title: "K",
+    description: "Knife",
+    assetId: "kangaroo_asset",
+    scale: "0.09 0.09 0.09",
+    scaleOne: "0.1 0.1 0.1",
+    scaleTwo: "0.13 0.13 0.13",
+    isObj: false,
   },
   {
     id: 12,
-    title: 'L',
-    description: 'Lion',
-    assetId: 'lion_asset',
-    scale: '0.01 0.01 0.01',
-    scaleOne: '0.012 0.012 0.012',
-    scaleTwo: '0.013 0.013 0.013',
-    isObj: false
+    title: "L",
+    description: "Lion",
+    assetId: "lion_asset",
+    scale: "0.01 0.01 0.01",
+    scaleOne: "0.012 0.012 0.012",
+    scaleTwo: "0.013 0.013 0.013",
+    isObj: false,
   },
   {
     id: 13,
-    title: 'M',
-    description: 'Monkey',
-    objAsset: 'monkey_asset_obj',
-    mtlAsset: 'monkey_asset_mtl',
-    scale: '0.5 0.5 0.5',
-    scaleOne: '0.6 0.6 0.6',
-    scaleTwo: '0.9 0.9 0.9',
-    isObj: true
+    title: "M",
+    description: "Monkey",
+    objAsset: "monkey_asset_obj",
+    mtlAsset: "monkey_asset_mtl",
+    scale: "0.5 0.5 0.5",
+    scaleOne: "0.6 0.6 0.6",
+    scaleTwo: "0.9 0.9 0.9",
+    isObj: true,
   },
   {
     id: 14,
-    title: 'N',
-    description: 'Nest',
-    objAsset: 'nest_asset_obj',
-    mtlAsset: 'nest_asset_mtl',
-    scale: '0.5 0.5 0.5',
-    scaleOne: '0.7 0.7 0.7',
-    scaleOne: '0.9 0.9 0.9',
-    isObj: true
+    title: "N",
+    description: "Nest",
+    objAsset: "nest_asset_obj",
+    mtlAsset: "nest_asset_mtl",
+    scale: "0.5 0.5 0.5",
+    scaleOne: "0.7 0.7 0.7",
+    scaleOne: "0.9 0.9 0.9",
+    isObj: true,
   },
   {
     id: 15,
-    title: 'O',
-    description: 'Octopus',
-    objAsset: 'octopus_asset_obj',
-    mtlAsset: 'octopus_asset_mtl',
-    scale: '1.9 1.9 1.9',
-    scaleOne: '2 2 2',
-    scaleTwo: '2.2 2.2 2.2',
-    isObj: true
-
+    title: "O",
+    description: "Octopus",
+    objAsset: "octopus_asset_obj",
+    mtlAsset: "octopus_asset_mtl",
+    scale: "1.9 1.9 1.9",
+    scaleOne: "2 2 2",
+    scaleTwo: "2.2 2.2 2.2",
+    isObj: true,
   },
   {
     id: 16,
-    title: 'P',
-    description: 'Pencil',
-    assetId: 'pencil_asset',
-    scale: '0.1 0.1 0.1',
-    scaleOne: '0.2 0.2 0.2',
-    scaleTwo: '0.3 0.3 0.3',
-    isObj: false
-
+    title: "P",
+    description: "Parrot",
+    assetId: "parrot_asset",
+    scale: "0.1 0.1 0.1",
+    scaleOne: "0.2 0.2 0.2",
+    scaleTwo: "0.3 0.3 0.3",
+    isObj: false,
   },
   {
     id: 17,
-    title: 'Q',
-    description: 'Qtip',
-    objAsset: 'qtip_asset_obj',
-    mtlAsset: 'qtip_asset_mtl',
-    scale: '1.4 1.4 1.4',
-    scaleOne: '1.9 1.9 1.9',
-    scaleTwo: '2.3 2.3 2.3',
-    isObj: true
+    title: "Q",
+    description: "Qtip",
+    objAsset: "qtip_asset_obj",
+    mtlAsset: "qtip_asset_mtl",
+    scale: "1.4 1.4 1.4",
+    scaleOne: "1.9 1.9 1.9",
+    scaleTwo: "2.3 2.3 2.3",
+    isObj: true,
   },
   {
     id: 18,
-    title: 'R',
-    description: 'Rat',
-    assetId: 'rat_asset',
-    scale: '0.5 0.5 0.5',
-    scaleOne: '0.7 0.7 0.7',
-    scaleTwo: '0.9 0.9 0.9',
-    isObj: false
-
+    title: "R",
+    description: "Rat",
+    assetId: "rat_asset",
+    scale: "0.5 0.5 0.5",
+    scaleOne: "0.7 0.7 0.7",
+    scaleTwo: "0.9 0.9 0.9",
+    isObj: false,
   },
 
   {
     id: 19,
-    title: 'S',
-    description: 'Scissor',
-    assetId: 'scissor_asset',
-    scale: '0.2 0.2 0.2',
-    scaleOne: '0.4 0.4 0.4',
-    scaleTwo: '0.6 0.6 0.6',
-    isObj: false
-
+    title: "S",
+    description: "Snake",
+    assetId: "snake_asset",
+    scale: "0.2 0.2 0.2",
+    scaleOne: "0.4 0.4 0.4",
+    scaleTwo: "0.6 0.6 0.6",
+    isObj: false,
   },
   {
     id: 20,
-    title: 'T',
-    description: 'Telescope',
-    objAsset: 'telescope_asset_obj',
-    mtlAsset: 'telescope_asset_mtl',
-    scale: '1.8 1.8 1.8',
-    scaleOne: '2 2 2',
-    scaleTwo: '2.2 2.2 2.2',
-    isObj: true
+    title: "T",
+    description: "Telescope",
+    objAsset: "telescope_asset_obj",
+    mtlAsset: "telescope_asset_mtl",
+    scale: "1.8 1.8 1.8",
+    scaleOne: "2 2 2",
+    scaleTwo: "2.2 2.2 2.2",
+    isObj: true,
   },
   {
     id: 21,
-    title: 'U',
-    description: 'Umbrella',
-    objAsset: 'umbrella_asset_obj',
-    mtlAsset: 'umbrella_asset_mtl',
-    scale: '0.010 0.010 0.010',
-    scaleOne: '0.015 0.015 0.015',
-    scaleTwo: '0.017 0.17 0.017',
-    isObj: true
-
+    title: "U",
+    description: "Umbrella",
+    objAsset: "umbrella_asset_obj",
+    mtlAsset: "umbrella_asset_mtl",
+    scale: "0.010 0.010 0.010",
+    scaleOne: "0.015 0.015 0.015",
+    scaleTwo: "0.017 0.17 0.017",
+    isObj: true,
   },
   {
     id: 22,
-    title: 'V',
-    description: 'Van',
-    assetId: 'van_asset',
-    objAsset: 'vase_asset_obj',
-    mtlAsset: 'vase_asset_mtl',
-    scale: '6 6 6',
-    scaleOne: '7 7 7 ',
-    scaleTwo: '8 8 8',
-    isObj: false
-
+    title: "V",
+    description: "Vase",
+    assetId: "vase_asset",
+    // objAsset: "vase_asset_obj",
+    // mtlAsset: "vase_asset_mtl",
+    scale: "6 6 6",
+    scaleOne: "7 7 7 ",
+    scaleTwo: "8 8 8",
+    isObj: false,
   },
   {
     id: 23,
-    title: 'W',
-    description: 'Whale',
-    assetId: 'whale_asset',
-    scale: '0.5 0.5 0.5',
-    scaleOne: '0.7 0.7 0.7',
-    scaleTwo: '1 1 1',
-    isObj: false
-
+    title: "W",
+    description: "Whale",
+    assetId: "whale_asset",
+    scale: "0.5 0.5 0.5",
+    scaleOne: "0.7 0.7 0.7",
+    scaleTwo: "1 1 1",
+    isObj: false,
   },
 
   {
     id: 24,
-    title: 'X',
-    description: 'Xmas',
-    objAsset: 'xmas_asset_obj',
-    mtlAsset: 'xmas_asset_mtl',
-    scale: '2.0 2.0 2.0',
-    scaleOne: '2.5 2.5 2.5',
-    scaleTwo: '2.8 2.8 2.8',
-    isObj: true
+    title: "X",
+    description: "Xmas",
+    objAsset: "xmas_asset_obj",
+    mtlAsset: "xmas_asset_mtl",
+    scale: "2.0 2.0 2.0",
+    scaleOne: "2.5 2.5 2.5",
+    scaleTwo: "2.8 2.8 2.8",
+    isObj: true,
   },
   {
     id: 25,
-    title: 'Y',
-    description: 'Yolk',
-    objAsset: 'yolk_asset_obj',
-    mtlAsset: 'yolk_asset_mtl',
-    scale: '0.03 0.03 0.03',
-    scaleOne: '0.05 0.05 0.05',
-    scaleTwo: '0.07 0.07 0.07',
-    isObj: true
-
+    title: "Y",
+    description: "Yolk",
+    objAsset: "yolk_asset_obj",
+    mtlAsset: "yolk_asset_mtl",
+    scale: "0.03 0.03 0.03",
+    scaleOne: "0.05 0.05 0.05",
+    scaleTwo: "0.07 0.07 0.07",
+    isObj: true,
   },
   {
     id: 26,
-    title: 'Z',
-    description: 'Zebra',
-    objAsset: 'zebra_asset_obj',
-    mtlAsset: 'zebra_asset_mtl',
-    scale: '0.1 0.1 0.1 ',
-    scaleOne: '0.3 0.3 0.3',
-    scaleTwo: '0.5 0.5 0.5',
-    isObj: true
-
-  }
+    title: "Z",
+    description: "Zebra",
+    objAsset: "zebra_asset_obj",
+    mtlAsset: "zebra_asset_mtl",
+    scale: "0.1 0.1 0.1 ",
+    scaleOne: "0.3 0.3 0.3",
+    scaleTwo: "0.5 0.5 0.5",
+    isObj: true,
+  },
 ];
 $(document).ready(init());
 //starting point
 function init() {
   var config = {
-    scaleFlag : 0,
+    scaleFlag: 0,
     isVolume: true,
     currentObject: {},
     currentId: 0,
-    prev: 0
-  }
-  var
-    rotateBtn = $(".rotate"),
+    prev: 0,
+  };
+  var rotateBtn = $(".rotate"),
     scaleBtn = $(".scale"),
     switchBtn = $(".switch"),
     gotitBtn = $(".gotit"),
@@ -302,27 +296,25 @@ function init() {
     nextBtn = $(".next"),
     volumeBtn = $("#volume");
 
-  
-    if (!'speechSynthesis' in window) {
-      // Synthesis support. show volume button
-      config.isVolume = false;
-      $('#volume').css('display', 'none');
-    }
+  if (!"speechSynthesis" in window) {
+    // Synthesis support. show volume button
+    config.isVolume = false;
+    $("#volume").css("display", "none");
+  }
   addObject(data, config.currentId, 1);
 
   gotitBtn.click(function () {
     gotitBtn.parent().css({
-      display: "none"
+      display: "none",
     });
   });
 
-
   rotateBtn.click(function () {
     if (rotateBtn.hasClass("rotate_fade")) {
-      config.currentObject.emit('endRotate');
+      config.currentObject.emit("endRotate");
       rotateBtn.removeClass("rotate_fade");
     } else {
-      config.currentObject.emit('rotate');
+      config.currentObject.emit("rotate");
       rotateBtn.addClass("rotate_fade");
     }
   });
@@ -334,16 +326,16 @@ function init() {
     scaleTwo = data[config.currentId].scaleTwo;
     if (config.scaleFlag == 0) {
       config.scaleFlag = 1;
-      config.currentObject.setAttribute("scale", scale = scaleOne);
+      config.currentObject.setAttribute("scale", (scale = scaleOne));
       scaleBtn.addClass("scale_one");
     } else if (config.scaleFlag == 1) {
       config.scaleFlag = 2;
-      config.currentObject.setAttribute("scale", scale = scaleTwo);
+      config.currentObject.setAttribute("scale", (scale = scaleTwo));
       scaleBtn.addClass("scale_two");
       scaleBtn.removeClass("scale_one");
     } else if (config.scaleFlag == 2) {
       config.scaleFlag = 0;
-      config.currentObject.setAttribute("scale", scale = scale);
+      config.currentObject.setAttribute("scale", (scale = scale));
       scaleBtn.removeClass("scale_two");
     }
   });
@@ -361,7 +353,7 @@ function init() {
 
     addObject(data, config.currentId, config.currentId + 1);
     hideObjects(data, config.currentId, config.currentId + 1);
-    $('.title').html(data[config.currentId].description);
+    $(".title").html(data[config.currentId].description);
     speak();
   });
 
@@ -377,68 +369,76 @@ function init() {
     }
     addObject(data, config.currentId, config.currentId - 1);
     hideObjects(data, config.currentId, config.currentId - 1);
-    $('.title').html(data[config.currentId].description);
+    $(".title").html(data[config.currentId].description);
     speak();
   });
 
   volumeBtn.click(function () {
     config.isVolume = !config.isVolume;
     if (config.isVolume) {
-      volumeBtn.removeClass('rotate_fade');
+      volumeBtn.removeClass("rotate_fade");
     } else {
-      volumeBtn.addClass('rotate_fade');
+      volumeBtn.addClass("rotate_fade");
     }
   });
 
   function addObject(data, toRemove, toRemoveId) {
     let currentData = data[config.currentId];
-    if (document.querySelector('#obj_' + currentData.id)) {
-      document.querySelector('#obj_' + currentData.id).setAttribute('visible', 'true');
+    if (document.querySelector("#obj_" + currentData.id)) {
+      document
+        .querySelector("#obj_" + currentData.id)
+        .setAttribute("visible", "true");
       return;
     }
 
-    let entity = document.createElement('a-entity');
-    entity.setAttribute('id', 'obj_' + currentData.id);
-    entity.setAttribute('rotation', '0 -90 0');
-    entity.setAttribute('side', 'double');
-    entity.setAttribute('position', '0.0 0.2 0');
-    entity.setAttribute('scale', currentData.scale);
+    let entity = document.createElement("a-entity");
+    entity.setAttribute("id", "obj_" + currentData.id);
+    entity.setAttribute("rotation", "0 -90 0");
+    entity.setAttribute("side", "double");
+    entity.setAttribute("position", "0.0 0.2 0");
+    entity.setAttribute("scale", currentData.scale);
     config.currentObject = entity;
     if (currentData.isObj === true) {
-      entity.setAttribute('obj-model', 'obj: #' + currentData.objAsset + '; mtl: #' + currentData.mtlAsset);
+      entity.setAttribute(
+        "obj-model",
+        "obj: #" + currentData.objAsset + "; mtl: #" + currentData.mtlAsset
+      );
     } else {
-      entity.setAttribute('gltf-model', '#' + currentData.assetId);
+      entity.setAttribute("gltf-model", "#" + currentData.assetId);
     }
 
     //Add animation
-    let animation = document.createElement('a-animation');
-    animation.setAttribute('attribute', 'rotation');
-    animation.setAttribute('begin', 'rotate');
-    animation.setAttribute('end', 'endRotate');
-    animation.setAttribute('dur', '26000');
-    animation.setAttribute('fill', 'forwards');
-    animation.setAttribute('to', '0 270 0');
-    animation.setAttribute('repeat', 'indefinite');
-    animation.setAttribute('easing', 'linear');
+    let animation = document.createElement("a-animation");
+    animation.setAttribute("attribute", "rotation");
+    animation.setAttribute("begin", "rotate");
+    animation.setAttribute("end", "endRotate");
+    animation.setAttribute("dur", "26000");
+    animation.setAttribute("fill", "forwards");
+    animation.setAttribute("to", "0 270 0");
+    animation.setAttribute("repeat", "indefinite");
+    animation.setAttribute("easing", "linear");
 
-    let marker = document.querySelector('#aframe-scene');
+    let marker = document.querySelector("#aframe-scene");
     entity.appendChild(animation);
     marker.appendChild(entity);
   }
 
   function hideObjects(data, currentId) {
     let current_id = currentId + 1;
-    if (document.querySelector('#obj_' + data.id)) {
-      document.querySelector('#obj_' + data.id).setAttribute('visible', 'true');
+    if (document.querySelector("#obj_" + data.id)) {
+      document.querySelector("#obj_" + data.id).setAttribute("visible", "true");
       //return;
     }
-    data.forEach(element => {
-      if (document.querySelector('#obj_' + element.id) !== null) {
+    data.forEach((element) => {
+      if (document.querySelector("#obj_" + element.id) !== null) {
         if (element.id === current_id) {
-          document.querySelector('#obj_' + element.id).setAttribute('visible', true);
+          document
+            .querySelector("#obj_" + element.id)
+            .setAttribute("visible", true);
         } else {
-
-          document.querySelector('#obj_' + element.id).setAttribute('visible', false);
+          document
+            .querySelector("#obj_" + element.id)
+            .setAttribute("visible", false);
         }
       }
     });
@@ -446,9 +446,12 @@ function init() {
   //speak feature
   function speak() {
     if (!config.isVolume) return;
-    let text = data[config.currentId].title + ' for ' + data[config.currentId].description;
+    let text =
+      data[config.currentId].title +
+      " for " +
+      data[config.currentId].description;
     let msg = new SpeechSynthesisUtterance();
-    msg.lang = 'en-US';
+    msg.lang = "en-US";
     msg.rate = 8 / 10;
     msg.pitch = 1;
     msg.text = text;
@@ -458,15 +461,19 @@ function init() {
 
 //Camera feature check
 function hasGetUserMedia() {
-  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia);
+  return !!(
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
+  );
 }
 
 if (hasGetUserMedia()) {
   // Good to go!
-  $('#splashScreen').css('display', 'block');
+  $("#splashScreen").css("display", "block");
 } else {
-  let template = '<p>This feature is not supported in your browser.</p>'
-  $('#noMedia').append(template);
-  $('#noMedia').css('display', 'block');
+  let template = "<p>This feature is not supported in your browser.</p>";
+  $("#noMedia").append(template);
+  $("#noMedia").css("display", "block");
 }
